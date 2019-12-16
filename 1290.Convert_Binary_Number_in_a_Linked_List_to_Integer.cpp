a5be74ac601cf339d7adcb5c3fbc91a455b6eb45
Joao Sousa <joaosousa.pt@gmail.com>
2019-12-16 11:54:03 +0000

    Create 1290.Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp

---
+++ 1290.Convert_Binary_Number_in_a_Linked_List_to_Integer.cpp
@@ -0,0 +1,30 @@
+/*
+Runtime: 4 ms, faster than 59.66% of C++ online submissions for Convert Binary Number in a Linked List to Integer.
+Memory Usage: 8.6 MB, less than 100.00% of C++ online submissions for Convert Binary Number in a Linked List to Integer.
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        vector<int> results;
+        int result = 0;
+        while(true) {
+            
+            results.insert(results.begin(), head->val);
+            if (head->next == NULL) break;
+            head = head->next;
+        }
+       
+        for (int i = 0; i < results.size(); ++i) {
+            result += results.at(i) * pow(2, i);
+        }
+        return result;
+    }
+};
