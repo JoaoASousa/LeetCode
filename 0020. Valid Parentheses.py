796fa81b90deb57709c96a6cecdf4ff6932f9142
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2023-06-11 11:29:40 +0100

    [0020] Added solution - Python

--- 0020. Valid Parentheses.py
+++ 0020. Valid Parentheses.py
@@ -1,4 +1,25 @@
 '''
+Runtime: 42 ms. Beats 68.49%.
+Memory: 16.2 MB. Beats 72.10%.
+'''
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapStartEnd = {")": "(", "]": "[", "}": "{"}
+
+        stack = []
+        
+        for char in s:
+            if char in mapStartEnd : # it is one of the closing brackets
+                if stack and stack[-1] == mapStartEnd[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+
+        return len(stack) == 0
+
+'''
 Runtime: 53 ms. Beats 13.67%.
 Memory: 16.5 MB. Beats 6.92%.
 '''
